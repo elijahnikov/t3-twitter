f3f5cab1e9901c4d63bef1c45d6b6f5e59f5ab07
Elijah Nikov <posnikovelijah@gmail.com>
2023-07-11 21:27:01 +0100

    post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,8 +16,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
