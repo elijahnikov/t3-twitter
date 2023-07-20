e441ee20913a64df7e9b217749a81f4c912dc07c
elijahnikov <posnikovelijah@gmail.com>
2023-07-20 23:04:19 +0100

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -21,13 +21,6 @@ const config = {
   images: {
     domains: ["images.clerk.dev"],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  swcMinify: true,
 };
 
 export default config;
